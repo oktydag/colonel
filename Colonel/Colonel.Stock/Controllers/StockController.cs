56df71d555a674c36c83dc49ae67c705af998794
oktaydagdelen <oktaydagdelen@OKTAYDADELE75E5>
2020-08-05 14:22:41 +0300

    mongodb connection configured and stock services added

---
+++ Colonel/Colonel.Stock/Controllers/StockController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Colonel.Stock.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colonel.Stock.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly StockService _stockService;
+
+        public StockController(StockService stockService)
+        {
+            _stockService = stockService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Stock>> Get() =>
+         _stockService.Get();
+
+    }
+}
---
+++ Colonel/Colonel.Stock/Services/StockService.cs
@@ -0,0 +1,29 @@
+﻿using Colonel.Stock.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Colonel.Stock.Services
+{
+    public class StockService
+    {
+        private readonly IMongoCollection<Stock> _stock;
+
+        public StockService(IStockDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _stock = database.GetCollection<Stock>(settings.StockCollectionName);
+        }
+
+
+        public List<Stock> Get() =>
+            _stock.Find(book => true).ToList();
+
+        public Stock Get(string id) =>
+            _stock.Find<Stock>(book => book.Id == id).FirstOrDefault();
+    }
+}
